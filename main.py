9e26d4c69d4ad9b2d6daee4708e508600af05e85
Alvaro Hernandez de los Reyes <alvaro.hernandez@inetum.com>
2022-05-10 20:15:26 +0200

    add connection optios

--- main.py
+++ main.py
@@ -1,18 +1,52 @@
+import argparse
 import pandas as pd
-import utils.connection as conn
+import time
+import utils.connection as connection
 
 # Create dataframe from json
 df = pd.read_csv("host-inventory.csv")
 # Set headers
 df = df[['host', 'ip', 'os']]
 # Filter by column value
-df = df[df['os'].str.contains("linux")]
-# Display the first 10 rows
-#df = df.head(10)
-# Export to csv
-#df.to_csv('out.csv', index = False)
-#print(df)
+df = df[df['os'].str.contains('linux')]
 
-for ip in df['ip']:
-    print(ip)
-    conn.ssh(ip,"user","password","hostname")
+# Test conection (for loop, better with apply; see add_ssh_key or ssh_key functions)
+def connection_test_iteration():
+    for ip in df['ip']:
+        connection.test(ip)
+
+# Test conection and save result to new column connection_test
+def connection_test():
+    df['connection_test'] = df['ip'].apply(connection.test)
+
+# Add ssh public key
+def add_ssh_key():
+    df['ip'].apply(connection.add_ssh_key)
+
+# Connect with ssh key
+def ssh_key():
+    df['ip'].apply(connection.ssh_key)
+
+def default():
+    print("Try '--help' for more information.")
+
+# Create switch_options dictionary
+switch_options = {
+    "connection_test_iteration": connection_test_iteration,
+    "connection_test": connection_test,
+    "add_ssh_key": add_ssh_key,
+    "ssh_key": ssh_key,
+}
+
+# Switch function
+def switch(option):
+    switch_options.get(option, default)()
+
+# Construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-e", "--execute", required=True, help="connection_test_iteration, connection_test, add_ssh_key, ssh_key")
+args = vars(ap.parse_args())
+
+# Execute switch case
+option = args['execute']
+switch(option)
--- utils/connection.py
+++ utils/connection.py
@@ -1,12 +1,66 @@
 import paramiko
 
-def ssh(ip,username,passwd,cmd):
+# Test conection and return boolean
+def test(ip):
+    user = "user"
+    password = "password"
     try:
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(ip,22,username,passwd,timeout=5)
+        ssh.connect(ip,22,user,password,timeout=5)
+        print('%s\tOK'%(ip))
+        return True
+    except:
+        print('%s\tKO'%(ip))
+        return False
+
+# Add ssh public key
+# You require id_rsa.pub in the project folder
+# Change de content of cmd variable
+def add_ssh_key(ip):
+    user = "user"
+    password = "password"
+
+    # Example of using the cmd, with different variables
+    # hola = "hola"
+    # adios = "adios"
+    # cmd = '''
+    # echo hola
+    # echo {}
+    # echo adios
+    # echo {}
+    # '''.format(hola,adios)
+
+    # Incorporate the id_rsa.pub in the authorized_keys dir
+    with open ("id_rsa.pub", "r") as myfile:
+        data = myfile.read().strip()
+    cmd = '''
+    mkdir -p /home/ahernandez/.ssh;touch /home/ahernandez/.ssh/authorized_keys
+    echo {} > /home/ahernandez/.ssh/authorized_keys
+    chown -R ahernandez:admindob /home/ahernandez
+    chmod 700 /home/ahernandez/.ssh
+    '''.format(data)
+
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ip,22,user,password,timeout=5)
         stdin,stdout,stderr = ssh.exec_command(cmd)
-        print('%s OK\n'%(ip))
+        # print(stderr.read())
         ssh.close()
     except :
-        print('%s KO\n' %(ip))
+        print('%s\tKO'%(ip))
+
+
+# Connect with ssh key
+def ssh_key(ip):
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=ip, port=22, username = "ahernandez", key_filename='id_rsa', timeout=5)
+        # stdin,stdout,stderr = ssh.exec_command("date")
+        # print(stderr.read())
+        print('%s\tOK'%(ip))
+        ssh.close()
+    except :
+        print('%s\tKO'%(ip))
